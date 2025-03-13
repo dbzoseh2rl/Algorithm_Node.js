e3263d3058b4fcfb9f3bedf2a9b96592e386330f
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-13 22:10:58 +0900

    feat: BOJ_B5_1001 add

--- BOJ/Bronze/B5/1001.js
+++ BOJ/Bronze/B5/1001.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
-let input = require('fs').readFileSync('input.txt').toString().split('\n');
+let input = fs.readFileSync('input.txt').toString().trim().split('\n');
+
 // input.txt 기준이 B4안에서 input.txt를 찾음 그래서 오류 계속 떳음;
 // const input = fs.readFileSync("/dev/stdin").toString().split(' ');
 
