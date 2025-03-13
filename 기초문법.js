a99fdb01e5291a3fcbea95d618fb485f5a319e3a
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-14 00:45:27 +0900

    feat: 기초문법.js add

---
+++ 기초문법.js
@@ -0,0 +1,47 @@
+console.log('hello');
+console.log('hello', 'world');
+
+
+/*
+variable -> 선언
+1) let ->
+2) const ->
+3) var -> 잘 안씀 요즘 ()
+*/
+
+var name ='ldk';
+console.log(name);
+
+let ive='아이브';
+console.log(ive);
+
+/* let 과 var로 선언하면 값을 추후에 변경가능 */
+
+ive = '안유진';
+console.log(ive);
+
+const newjeans ='뉴진스';
+console.log(newjeans);
+
+
+/*
+1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 사용할 수 있다.
+2) 특수기호는 언더바와 달러만 사용가능
+3) 숫자로 이름 시작 불가
+4) 키워드는 변수명으로 선언 불가
+*/
+
+/*
+Map = key:value
+*/
+
+const dictionary ={
+    red: '빨',
+    blue: '파',
+};
+
+console.log(dictionary);
+console.log(dictionary['red']);
+
+
+
