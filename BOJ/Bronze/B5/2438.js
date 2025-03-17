bbdf6f74ecaa442d20b3056f5f7609a3d41fb5b7
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-18 00:20:53 +0900

    fix: BOJ_B5_2438 풀이방안 수정

--- BOJ/Bronze/B5/2438.js
+++ BOJ/Bronze/B5/2438.js
@@ -1,17 +1,17 @@
 // 동기식 처리 (백준 제출)
-const fs = require("fs");
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-// const input = require('fs')
-//     .readFileSync('input.txt')
-//     .toString()
-//     .trim()
-//     .split('\r\n');
+// const fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// // const input = require('fs')
+// //     .readFileSync('input.txt')
+// //     .toString()
+// //     .trim()
+// //     .split('\r\n');
 
-const N = parseInt(input[0]);
+// const N = parseInt(input[0]);
 
-for (let i = 1; i <= N; i++){
-    console.log("*".repeat(i));
-}
+// for (let i = 1; i <= N; i++){
+//     console.log("*".repeat(i));
+// }
 
 // 비동기식 (내가 짠거)
 process.stdin.setEncoding('utf8');
