24d9714adb3ccb8a63c1c8ed681576d858267ebe
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-18 00:11:21 +0900

    feat: BOJ_B5_2438 비동기,동기 add

--- BOJ/Bronze/B5/2438.js
+++ BOJ/Bronze/B5/2438.js
@@ -1,14 +1,46 @@
-const input = require('fs')
-    .readFileSync('input.txt')
-    .toString()
-    .trim()
-    .split('\r\n');
+// 동기식 처리 (백준 제출)
+const fs = require("fs");
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = require('fs')
+//     .readFileSync('input.txt')
+//     .toString()
+//     .trim()
+//     .split('\r\n');
 
-console.log(input);
-
-const N = parseInt(input);
+const N = parseInt(input[0]);
 
 for (let i = 1; i <= N; i++){
     console.log("*".repeat(i));
 }
 
+// 비동기식 (내가 짠거)
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const input = require('fs')
+                .readFileSync('input.txt')
+                .toString()
+                .trim()
+                .split('\r\n');
+
+    const N = parseInt(input[0]); // 첫 번째 줄의 값을 N으로 파싱
+
+    for (let i = 1; i <= N; i++) {
+        console.log("*".repeat(i)); // 별표 출력
+    }
+
+    process.exit();
+});
+
+// 비동기 권장 코드
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const input = data.trim().split('\n');
+    const N = parseInt(input[0]);
+
+    for (let i = 1; i <= N; i++) {
+        console.log("*".repeat(i));
+    }
+});
