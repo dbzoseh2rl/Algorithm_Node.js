1166c6dfbd2fdab24c2cf302f0a832de2cc7e68e
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-22 20:08:53 +0900

    fix: BOJ_B5_2741 add

--- BOJ/Silver/S5/1268.js
+++ BOJ/Silver/S5/1268.js
@@ -6,6 +6,7 @@ const input = require('fs')
 
 let size = parseInt(input[0]);
 
+// 배열에 담음;
 const student = [];
 for (let i = 1; i <= size; i++){
     student.push(input[i].split(' ').map(Number));
@@ -13,6 +14,15 @@ for (let i = 1; i <= size; i++){
 
 console.log(student)
 
+function solution(student){
+    
+}
+
+solution(student)
+
+// for (let i = 1; i <= size; i++) {
+//     console.log(input[i].split(" "));
+// }
 
 // for (let i = 0; i< r1; i++) {
 //     for (let j = 0; j < r1; j++) {
