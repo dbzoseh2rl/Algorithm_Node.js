f3671f76dfba8a3343466a4cb94e338a1394ba90
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-24 00:03:48 +0900

    Update: BOJ_Bronze_B5

--- BOJ/Bronze/B5/25372.js
+++ BOJ/Bronze/B5/25372.js
@@ -1,19 +1,31 @@
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-let input = [];
+// let input = [];
 
-rl.on("line", (line) => {
-    input.push(line);
-}).on("close", () => {
-    const T = parseInt(input[0]);
-    for (let i = 1; i <= T; i++) {
-        let passwordLength = input[i].length;
-        console.log(passwordLength >= 6 && passwordLength <= 9 ? "yes" : "no");
-    }
-    process.exit();
-});
+// rl.on("line", (line) => {
+//     input.push(line);
+// }).on("close", () => {
+//     const T = parseInt(input[0]);
+//     for (let i = 1; i <= T; i++) {
+//         let passwordLength = input[i].length;
+//         console.log(passwordLength >= 6 && passwordLength <= 9 ? "yes" : "no");
+//     }
+//     process.exit();
+// });
+
+
+//짜본코드
+const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("input.txt").toString().trim().split("\n");
+const T = parseInt(input[0]);
+
+for (let i = 1; i <= T; i++) {
+    let passwordLength = input[i].length;
+    console.log(passwordLength >= 6 && passwordLength <= 9 ? "yes" : "no");
+}
