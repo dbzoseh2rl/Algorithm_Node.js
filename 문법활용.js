22f11bf8621d0dfcdc443439e9b148201e715919
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-06 23:33:17 +0900

    Update: 문법활용

--- 문법활용.js
+++ 문법활용.js
@@ -1,3 +1,4 @@
+/* 
 // const fs = require("fs"); // fs 모듈 추가
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
@@ -30,7 +31,7 @@ const sumvalue = values.reduce((acc,cur) => acc + cur, 0);
 // 원형 = array.reduce((accumulator, currentValue) => { ... }, initialValue);
 // const sumvalue = values.reduce((acc,cur) => acc + cur, 0);
 // reduce는 숫자를 반환해주기 때문에 굳이 parseInt안해도됨
-
+ */
 
 
 
@@ -39,3 +40,37 @@ const arr = [[1, 2], [3, 4], [5, 6]];
 const result = arr.flatMap(innerArray => innerArray.map(num => num * 2));
 console.log(result); // [2, 4, 6, 8, 10, 12]
 
+
+// // const fs = require("fs"); // fs 모듈 추가
+// // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// const input = require('fs')
+//     .readFileSync('input.txt')
+//     .toString()
+//     .trim()
+//     .split('\r\n');
+
+// const N = parseInt(input[0]);
+// // console.log(N)
+
+// for(let i = 0; i < N; i++){
+//     console.log(" ".repeat(i) + "*".repeat(N-i));
+// }
+
+
+
+// Method Chaning 방식 
+// // const fs = require("fs"); // fs 모듈 추가
+// // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [input] = require('fs')
+    .readFileSync('input.txt')
+    .toString()
+    .trim()
+    .split('\r\n');
+const N = parseInt(input);  
+
+// Array.from을 사용하여 길이 N의 배열을 생성하고,
+// map과 forEach를 체인하여 패턴을 출력합니다.
+Array.from({ length: N })
+  .map((_, i) => " ".repeat(i) + "*".repeat(N - i))
+  .forEach(line => console.log(line));
